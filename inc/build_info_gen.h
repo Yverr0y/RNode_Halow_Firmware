73295c075839282b0f230f412d1da870f003eea8
I-AM-ENGINEER <61810711+I-AM-ENGINEER@users.noreply.github.com>
2026-08-23 00:19:06 +0300

    chore: build 212, eide upload command points at utils/flash.py

--- inc/build_info_gen.h
+++ inc/build_info_gen.h
@@ -3,8 +3,8 @@
 #define __BUILD_INFO_GEN_H__
 #include "sys_config.h"
 #define FW_BUILD_BETA       1
-#define FW_BUILD_NUMBER     199
-#define FW_BUILD_NUMBER_STR "199"
-#define FW_BUILD_DATE       "2026-08-21 02:03:57"
+#define FW_BUILD_NUMBER     212
+#define FW_BUILD_NUMBER_STR "212"
+#define FW_BUILD_DATE       "2026-08-23 00:12:59"
 #define FW_BUILD_VERSION    FW_VERSION "b" FW_BUILD_NUMBER_STR " (" FW_BUILD_DATE ")"
 #endif
